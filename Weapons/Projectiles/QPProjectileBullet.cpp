326145e5cfc1a765168c83853fdb789c092a0cbe
Min <rbals418@naver.com>
2026-01-20 14:19:16 +0900

    Projectiles file include QPProjectileBullet source file

---
+++ Weapons/Projectiles/QPProjectileBullet.cpp
@@ -0,0 +1,74 @@
+#include "QPProjectileBullet.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "DrawDebugHelpers.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include "NiagaraSystem.h"
+
+AQPProjectileBullet::AQPProjectileBullet()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	BulletCollision = CreateDefaultSubobject<USphereComponent>(TEXT("BulletCollision")); //총알 충돌 컴포넌트 생성
+	SetRootComponent(BulletCollision); //루트 컴포넌트로 설정
+	BulletCollision->InitSphereRadius(5.f); //충돌 반지름 설정
+	BulletCollision->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics); //충돌 활성화 설정
+	BulletCollision->SetCollisionResponseToAllChannels(ECR_Block); //모든 채널에 대해 충돌 응답 설정
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement")); //총알 이동 컴포넌트 생성
+	ProjectileMovement->bRotationFollowsVelocity = true; //회전이 속도를 따르도록 설정
+	ProjectileMovement->ProjectileGravityScale = 1.0f; //중력 스케일 설정
+
+	//총알 메쉬 컴포넌트 생성
+	BulletMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BulletMesh"));
+	BulletMesh->SetupAttachment(RootComponent); //루트 컴포넌트에
+	BulletMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics); //충돌 비활성화 설정
+	InitialLifeSpan = 5.0f; //수명 설정
+}
+
+void AQPProjectileBullet::SetBulletVelocity(const FVector& Direction, float Speed)
+{
+	if (ProjectileMovement) {
+		const FVector CommonVelocity = Direction.GetSafeNormal() * Speed; //방향을 정규화하고 속도 곱하기
+		ProjectileMovement->Velocity = CommonVelocity; //총알 이동 컴포넌트
+		ProjectileMovement->InitialSpeed = Speed; //초기 속도 설정
+		ProjectileMovement->MaxSpeed = Speed; //최대 속도 설정
+	}
+}
+
+void AQPProjectileBullet::BeginPlay()
+{
+	Super::BeginPlay();
+	PrevLocation = GetActorLocation(); //이전 위치 초기화
+	if (AActor* OwnerActor = GetOwner())
+	{
+		//총알이 소유자와 충돌하지 않도록 설정
+		BulletCollision->IgnoreActorWhenMoving(OwnerActor, true); //소유자 무시
+	}
+	if (APawn* InstigatorPawn = GetInstigator())
+	{
+		//총알이 인스티게이터와 충돌하지 않도록 설정
+		BulletCollision->IgnoreActorWhenMoving(InstigatorPawn, true); //인스티게이터 무시
+	}
+
+	if (TrailFX) {
+		TrailFXComponent = UNiagaraFunctionLibrary::SpawnSystemAttached(TrailFX, RootComponent, NAME_None, FVector::ZeroVector, FRotator::ZeroRotator,
+			EAttachLocation::SnapToTarget, true, true, ENCPoolMethod::AutoRelease, true);  //트레일 이펙트 생성 및 부착
+	}
+	if (TrailFXComponent) {
+		TrailFXComponent->SetWorldScale3D(TrailFXScale);  //트레일 이펙트 스케일 설정
+		bDebugDrawTracer = false;
+	}
+}
+
+void AQPProjectileBullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (bDebugDrawTracer) {
+		const FVector CurrentLocation = GetActorLocation(); //현재 위치 가져오기
+		DrawDebugLine(GetWorld(), PrevLocation, CurrentLocation, FColor::Green, false, DebugSegmentLifeTime, 0, DebugThickness); //디버그 선 그리기
+		PrevLocation = CurrentLocation; //이전 위치 업데이트
+	}
+}
+
---
+++ Weapons/Projectiles/QPProjectileBullet.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "QPProjectileBullet.generated.h"
+
+UCLASS()
+class PJ_QUIET_PROTOCOL_API AQPProjectileBullet : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AQPProjectileBullet();
+	void SetBulletVelocity(const FVector& Direction, float Speed); //총알 속도 설정 함수
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere, Category = "Bullet")
+	TObjectPtr<class USphereComponent> BulletCollision; //총알 충돌 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = "Bullet")
+	TObjectPtr<class UProjectileMovementComponent> ProjectileMovement; //총알 이동 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = "Bullet")
+	TObjectPtr<class USkeletalMeshComponent> BulletMesh; //총알 메쉬 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bullet|FX")
+	TObjectPtr<class UNiagaraSystem> TrailFX = nullptr; //충돌 이펙트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bullet|FX")
+	FVector TrailFXScale = FVector(1.0f, 1.0f, 1.0f); //트레일 이펙트 스케일
+	UPROPERTY(Transient) //임시
+		TObjectPtr<class UNiagaraComponent> TrailFXComponent = nullptr; //트레일
+	//총알 Tracer 대체 임시 디버그	변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet|Debug", meta = (AllowPrivateAcess = "true"))
+	bool bDebugDrawTracer = true; //디버그 트레이서 그리기 여부
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet|Debug", meta = (AllowPrivateAcess = "true"))
+	float DebugSegmentLifeTime = 1.0f; //디버그 세그먼트 수명
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet|Debug", meta = (AllowPrivateAcess = "true"))
+	float DebugThickness = 2.0f; //디버그 두께
+
+	FVector PrevLocation = FVector::ZeroVector; //이전 위치
+
+};
